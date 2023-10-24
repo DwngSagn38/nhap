d81475bb4a5f6cab928392e37566b7dfc87e5a1f
DANG SANG <sangldph42693@fpt.edu.vn>
2023-10-24 14:27:30 +0700

    sang code layout

--- app/src/main/java/com/example/da_ql_khohang/ThongKe/Frag_Statistical.java
+++ app/src/main/java/com/example/da_ql_khohang/ThongKe/Frag_Statistical.java
@@ -5,10 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.da_ql_khohang.R;
+import com.example.da_ql_khohang.ThongKe.AdapterTK.adapterThongKe;
 import com.example.da_ql_khohang.databinding.FragProdBinding;
 import com.example.da_ql_khohang.databinding.FragStatisticalBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 
 public class Frag_Statistical extends Fragment {
@@ -24,13 +30,29 @@ public class Frag_Statistical extends Fragment {
         return fragment;
     }
 
+    private ViewPager2 viewPager2;
+    private TabLayout tabLayout;
+    adapterThongKe adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragStatisticalBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View view = inflater.inflate(R.layout.frag_statistical,container,false);
+        viewPager2 = view.findViewById(R.id.viewpage2);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        adapter = new adapterThongKe(getActivity());
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if (position == 0){
+                    tab.setText("Thống kê số lượng");
+                } else if (position == 1) {
+                    tab.setText("Thống kê lượng tiền");
+                }
+            }
+        }).attach();
+
+        return view;
     }
-
-
-
-
 }

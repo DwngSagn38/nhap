9e8d254a940092f4a0fd00a7cb5850befc0cf968
VanAnh <anhvtvph42837@fpt.edu.vn>
2023-10-24 14:24:00 +0700

    vanh code layout

---
+++ app/src/main/java/com/example/da_ql_khohang/Sign_in.java
@@ -0,0 +1,14 @@
+package com.example.da_ql_khohang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
